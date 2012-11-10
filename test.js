44528f391399d0c230009b1091e54b9b2c0e719d
Itay Neeman <itay@neeman.net>
2012-11-10 03:04:26 -0800

    Checkpoint debugger work

--- test.js
+++ test.js
@@ -17,13 +17,47 @@ process.on("exit", function() {
   debuggee.kill();
 });
 
+process.foo = "HELLO WORLD";
+process.bar = "bar";
+
 var DebugTranslator = require('./lib/debugTranslator').debugTranslator;
 var translator = new DebugTranslator(debuggee.pid);
 translator.connect(function() {
   console.log("CONNECTED", arguments);
+  
+  translator.setBreakpoint('/Users/ineeman/Work/pandabits/test/test.js', 3, function() {
+    //console.log(arguments);
+  });
+  translator.cont(function() {
+    translator.onBreak(function() {
+      translator.backtrace({}, function(resp, refs) {
+        console.log("bt", resp);
+      })
+    });
+  });
+  
+  //translator.onBreak();
+  //translator.cont();
   setTimeout(function() {
-    translator.execGlobal("console", function(err, resp) {
-      console.log(err, resp);
-    })
-  }, 1000);
+      
+      translator.scripts(function(scripts) {
+        console.log("scripts", scripts.map(function(script) { return script.name}));
+      })
+      
+      /*translator.break_(function(){
+        console.log("BREAK", arguments);
+        translator.evaluateGlobal("process.foobar", function(resp) {
+          var res = resp[0];
+          
+          translator.mirrorObject(res, 3, function(err, res) {
+            console.log("mirror", err, res);
+          });
+          
+          translator.backtrace({}, function() {
+            console.log("bt", arguments);
+          })
+        })
+        
+      });*/
+  }, 1000)
 });
--- test/test.js
+++ test/test.js
@@ -1,5 +1,18 @@
+function test2(counter) {
+    var a = 1;
+    console.log("Counter: ", counter);
+}
 
+function test1(counter) {
+    test2(counter);
+}
+
+process.foo = "foooo!!!";
+process.bar = [1,2,3,4];
+process.baz = { a: 2, b: 3};
+process.foobar = { a: { b: [1,2], c: 3}};
+console.log(process.foo);
 var counter = 0;
 setInterval(function() {
-    console.log("Counter: ", counter++);
+    test1(counter++);
 }, 1000);
