753a0fc331fefa38f907252d8e2907ab2b5641db
Aurojit Panda <apanda@cs.berkeley.edu>
2012-11-10 20:34:23 -0800

    Working redirection

--- test/test2.js
+++ test/test2.js
@@ -14,7 +14,10 @@ process.bar = [1,2,3,4];
 process.baz = { a: 2, b: 3};
 process.foobar = { a: { b: [1,2], c: 3}};
 console.log(process.foo);
-(function() {var fs = require("fs"); var f = fs.openSync("tmp","w"); process.stdout.write = (function (write) {return function(string, encoding, fd) {fs.writeSync(f, "" + string); }})(process.stdout.write)})();
+(function() {var fs = require("fs"); var f = fs.openSync("tmp","w"); 
+			process.stdout.write = (function (write) {
+				return function(string, encoding, fd) {fs.writeSync(f, "" + string);
+								write.call(process.stdout, string, encoding, fd); }})(process.stdout.write)})();
 var counter = 0;
 setInterval(function() {
     test1(counter++);
